91766d246f0ad330c961dd66015ef4a0b55456e0
y9413sh <y9413sh@gmail.com>
2021-10-27 16:09:55 +0900

    fix(front-frame): Login, Register 수정

--- front-end/src/components/user/Register.vue
+++ front-end/src/components/user/Register.vue
@@ -1,10 +1,17 @@
 <template>
 <div>
-    <!-- 회원가입 첫페이지  -->
-    <div class="outterborder" v-if="!isStep2">
-        <h2>Welcome Spring Coffee!</h2>
-        <br />
+  <div class="tab-bar">
+    <div class="tab-shop py-3" style="width:100%;">
+        <span class="tab-title">회원가입</span>            
+    </div>
+  </div>
+  <div class="py-2"><br></div>
+  <span class="qna"><i class="bi bi-shield-lock-fill"></i></span><br>
+  <span style="font-size: 11pt;">Spring Coffee 의 회원이 되어보세요!</span><br>
+  <span style="font-size: 11pt;">다양한 이벤트가 기다리고 있습니다!</span>
+  
 
+    <div class="outterborder" v-if="!isStep2">
         <div class="mb-3">
             <label for="email" class="form-label">Email address </label>
             <input  v-model="email"
@@ -124,8 +131,35 @@ export default {
 </script>
 
 <style scoped>
+.tab-bar {
+  background-image: url('../.././assets/background.jpg');
+  background-repeat : no-repeat;
+  background-size : cover;
+  position: relative;
+}
+
+.tab-shop {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.tab-title {
+    font-size: 22pt;
+    color: white;
+}
+
+.qna {
+    font-size: 26pt;
+}
+
+.qna:hover {
+    color: green;
+    font-size: 26pt;
+    transition: 0.3s;
+}
+
 .outterborder{
-  width: 30%;
+  width: 40%;
   margin: auto;
+  padding: 20px;
 }
 </style>
