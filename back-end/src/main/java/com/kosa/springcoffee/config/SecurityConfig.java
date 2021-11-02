3ec7db93f10dcc31e0f1579e9f2fe9316d3ab449
이충무 <lcm2822@daum.net>
2021-11-02 11:56:07 +0900

    1. feature(board): SecurityConfig 내용 추가 ApiCheckFilter 적용, ApiLoginFilter 적용 로그인 실패 핸들러 추가 JWT 토큰 생성 및 검증 추가 pom.xml jwt 추가

--- back-end/src/main/java/com/kosa/springcoffee/config/SecurityConfig.java
+++ back-end/src/main/java/com/kosa/springcoffee/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.kosa.springcoffee.config;
 
 
+
+
+import com.kosa.springcoffee.security.filter.ApiCheckFilter;
+import com.kosa.springcoffee.security.filter.ApiLoginFilter;
+import com.kosa.springcoffee.security.handler.ApiLoginFailHandler;
+import com.kosa.springcoffee.security.util.JWTUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @Log4j2
@@ -19,22 +27,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public ApiCheckFilter apiCheckFilter(){
+        return new ApiCheckFilter("/boards/**/*", jwtUtil());
+    }
+
+    @Bean
+    public JWTUtil jwtUtil(){
+        return new JWTUtil();
+    }
+
+    @Bean
+    public ApiLoginFilter apiLoginFilter() throws Exception{
+        ApiLoginFilter apiLoginFilter = new ApiLoginFilter("/login", jwtUtil());
+        apiLoginFilter.setAuthenticationManager(authenticationManager());
+
+        apiLoginFilter.setAuthenticationFailureHandler(new ApiLoginFailHandler());
+        return apiLoginFilter;
+    }
+
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/sample/all", "/signUp").permitAll();//로그인 없이 접근 가능
-                //.antMatchers("/sample/member").hasRole("USER");
-                //.antMatchers("/signUp").anonymous()
-//                .and()
-//                .formLogin()
-//                .and()
-//                .csrf().disable()//로그인 창;
-//                .logout();
-                        //.antMatchers("/sample/admin").hasRole("ADMIN");
+                .antMatchers("/sample/all", "/signUp").permitAll();
 
-        http.formLogin();//상위 접근권한 접근 문제시 로그인 화면으로 되돌아옴
+        http.formLogin();
         http.csrf().disable();
         http.logout();
+
+
+        http.addFilterBefore(apiCheckFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(apiLoginFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
 
--- back-end/src/main/java/com/kosa/springcoffee/service/BoardService.java
+++ back-end/src/main/java/com/kosa/springcoffee/service/BoardService.java
@@ -45,4 +45,5 @@ public interface BoardService {
                 .build();
         return dto;
     }
+
 }
