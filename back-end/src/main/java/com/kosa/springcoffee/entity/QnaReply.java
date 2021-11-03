fd2cfe63c1880a1804cd3bea394090ac6fbdf592
YejiKim <kej980324@gmail.com>
2021-11-03 17:45:21 +0900

    feature(qna-board): qna 댓글 entity 추가

---
+++ back-end/src/main/java/com/kosa/springcoffee/entity/QnaReply.java
@@ -0,0 +1,26 @@
+package com.kosa.springcoffee.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity(name = "sc_qna_reply")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class QnaReply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long replyNo;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member replyer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private QnaBoard qnaBoard;
+}
