f7d62be992f6285c9f94e33c2a219cba93c63c23
YejiKim <kej980324@gmail.com>
2021-11-11 16:29:34 +0900

    fix(jwt): Member entity UserDetails 구현, SecurityConfig 수정

--- back-end/src/main/java/com/kosa/springcoffee/config/SecurityConfig.java
+++ back-end/src/main/java/com/kosa/springcoffee/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Log4j2
@@ -18,6 +20,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     @Override
--- back-end/src/main/java/com/kosa/springcoffee/entity/Member.java
+++ back-end/src/main/java/com/kosa/springcoffee/entity/Member.java
@@ -2,13 +2,18 @@ package com.kosa.springcoffee.entity;
 
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity(name = "sc_member")
 @Builder
@@ -17,7 +22,7 @@ import java.util.Set;
 @Getter
 @ToString
 @Setter
-public class Member extends BaseEntity{
+public class Member extends BaseEntity implements UserDetails {
     @Id
     private String email;
 
@@ -29,15 +34,46 @@ public class Member extends BaseEntity{
 
     @ElementCollection(fetch = FetchType.LAZY)
     @Builder.Default
-    private Set<MemberRole> roleSet = new HashSet<>();
-
+    private List<MemberRole> roles = new ArrayList<>();
 
     //@OneToMany(mappedBy = "member")
     //private List<Order> orders = new ArrayList<>();
 
-
     public void addMemberRole(MemberRole memberRole){
-        roleSet.add(memberRole);
+        roles.add(memberRole);
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<String> roleList = new ArrayList<>();
+        this.roles.forEach(role -> roleList.add(role.name()));
+
+        return roleList.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
